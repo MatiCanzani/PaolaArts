aea906d5b1441f6da5b523749d05c0e91cb29bde
MatiCanzani <matiascanzani@gmail.com>
2023-10-02 22:36:08 +0100

    add bkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify/functions';
 
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
